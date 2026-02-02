da06a197c5be7a30c17d32e214512694e4bf53f1
sdsdsdsdqq <thd4090@naver.com>
2026-02-02 11:05:20 +0900

    Add Tworld

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-12">
           <Link
-            href="/chat"
+            href="/tworld"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
             <h2 className="mb-3 text-2xl font-semibold">
